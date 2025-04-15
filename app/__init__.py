096d78d09e92f3e5462d0f43c045143a80eb1b64
Roberta-Andreea Popescu <86193211+popescuroberta@users.noreply.github.com>
2025-04-15 21:39:07 +0300

    Create __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from config import Config
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+    
+    db.init_app(app)
+    login_manager.init_app(app)
+    
+    from app.routes import main_routes, auth_routes, product_routes
+    app.register_blueprint(main_routes)
+    app.register_blueprint(auth_routes)
+    app.register_blueprint(product_routes)
+    
+    return app
+
+from app import models
