d9541674a7ed634d5e2bb7cbc9d604a99666779d
Roberta-Andreea Popescu <86193211+popescuroberta@users.noreply.github.com>
2025-04-15 21:41:00 +0300

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,29 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, DecimalField, FileField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                                   validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class ProductForm(FlaskForm):
+    name = StringField('Product Name', validators=[DataRequired()])
+    description = TextAreaField('Description')
+    price = DecimalField('Price', validators=[DataRequired()])
+    material = StringField('Material')
+    weight = StringField('Weight')
+    carat = StringField('Carat')
+    color = StringField('Color')
+    clarity = StringField('Clarity')
+    country_of_origin = StringField('Country of Origin')
+    image = FileField('Product Image')
+    submit = SubmitField('Upload Product')
